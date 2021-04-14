4c82ec7c2d328f287956896250fda6c38c1558ea
Alexandr <44836865+RedBull4@users.noreply.github.com>
2021-04-14 22:29:42 +0400

    Create Java_Stdin_and_Stdout.java

---
+++ Java_Stdin_and_Stdout.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = 0, b = 0, c = 0;
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
